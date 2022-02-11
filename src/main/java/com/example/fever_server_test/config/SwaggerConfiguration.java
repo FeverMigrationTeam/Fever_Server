29286434c02cf6a768eb20bdb8997dea914475cd
TonyJHC <jongvin01@naver.com>
2022-02-11 23:26:33 +0900

    Swagger 설정

---
+++ src/main/java/com/example/fever_server_test/config/SwaggerConfiguration.java
@@ -0,0 +1,39 @@
+package com.example.fever_server_test.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket apis(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("Swagger Test")
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.fever_server_test"))
+//                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Fever 2.0")
+                .description("Fever APIs")
+                .version("2.0")
+                .build();
+    }
+
+}
