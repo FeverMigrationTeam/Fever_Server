846f0652a3e437f322b179b9e1ebafe31b66f69a
TonyJHC <jongvin01@naver.com>
2022-02-13 02:28:48 +0900

    모든 구장 검색 , 구장 검색 api

---
+++ src/main/java/com/example/fever_server_test/config/BeanConfig.java
@@ -0,0 +1,14 @@
+package com.example.fever_server_test.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
