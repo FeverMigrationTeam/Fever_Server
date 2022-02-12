a5fbbc3096d4bfe796484fa4ba7c5d9713f446e0
jinhyeok15 <ekfrhd97@gmail.com>
2022-02-12 12:40:52 +0900

    video/start API 설계 (1)

--- src/main/java/com/example/fever_server_test/controller/VideoController.java
+++ src/main/java/com/example/fever_server_test/controller/VideoController.java
@@ -1,12 +1,14 @@
 package com.example.fever_server_test.controller;
 
+import com.example.fever_server_test.dto.request.VideoStartReqDto;
+import com.example.fever_server_test.response.NoDataResponse;
 import com.example.fever_server_test.service.VideoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +21,10 @@ public class VideoController {
     @GetMapping("/")
     public String greeting() {
         return videoService.greet();
+    }
+
+    @PostMapping("/start")
+    public ResponseEntity<NoDataResponse> startVideo(@RequestHeader("Authorization") String token, VideoStartReqDto videoStartReqDto) {
+        return videoService.startVideo(token, videoStartReqDto);
     }
 }
--- src/main/java/com/example/fever_server_test/service/VideoService.java
+++ src/main/java/com/example/fever_server_test/service/VideoService.java
@@ -1,9 +1,14 @@
 package com.example.fever_server_test.service;
 
+import com.example.fever_server_test.dto.request.VideoStartReqDto;
 import com.example.fever_server_test.repository.VideoRepository;
+import com.example.fever_server_test.response.NoDataResponse;
 import com.example.fever_server_test.response.ResponseMessage;
 import com.example.fever_server_test.response.Status;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +18,17 @@ public class VideoService {
     private final ResponseMessage message;
     private final VideoRepository videoRepository;
 
+    @Autowired
+    private OauthService oauthService;
+
     public String greet() {
         return "Hello, World";
+    }
+
+    public ResponseEntity<NoDataResponse> startVideo(String token, VideoStartReqDto videoStartReqDto) {
+        String body = oauthService.requestSocialData(videoStartReqDto.getLoginType(), token);
+
+        return new ResponseEntity<NoDataResponse>(
+                NoDataResponse.response(200, "SUCCESS"), HttpStatus.OK);
     }
 }
